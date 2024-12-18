5c9f3b49db47f3d2014f8ed87bf965851d170663
Priyanshu-bhati23 <priyanshubhati23122005@gmail.com>
2024-12-18 12:23:04 +0530

    New Commits

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -6,7 +6,11 @@ contract SimpleStorage {
     
     uint256 public favouriteNumber; // Default value is 0
 
+//dyanmic array
     uint256[] public listOfFavouriteNumbers;
+
+    //mapping
+    mapping(string => uint256) public nameToFavouriteNumber;
 
     struct Person {
         uint256 myFavouriteNumber;
@@ -16,9 +20,11 @@ contract SimpleStorage {
     Person[] public people; // Array to store the list of people
 
     // Function to add a person to the list
+    
     function personList(uint256 _myFavouriteNumber, string memory _name) public {
         Person memory newPerson = Person(_myFavouriteNumber, _name);
         people.push(newPerson); // Store the person in the array
+        nameToFavouriteNumber[_name]=_myFavouriteNumber;
     }
 
     // Function to store a favourite number
@@ -28,10 +34,15 @@ contract SimpleStorage {
     }
 
     // Function to retrieve the favourite number
+    //view,pure
     function retrieve() public view returns (uint256) {
         return favouriteNumber;
     }
 }
+
+//memory is stored temporily,can be changed
+//call data cannot be changed,stored temporily
+//storage is stored permanently,can be changed
 
 
 //0xd9145CCE52D386f254917e481eB44e9943F39138
