48715cf3dc35339de0dc990ec27233ba67403c33
Priyanshu-bhati23 <priyanshubhati23122005@gmail.com>
2024-12-24 12:27:31 +0530

    .

--- FundMePro/FundMe.sol
+++ FundMePro/FundMe.sol
@@ -4,15 +4,19 @@ pragma solidity ^0.8.19;
 
 //GET FUNDS FROM USERS
 //WITHDRAW FUNDS
-//SET  A MINIMUM FUNDING VALUE IN USD
+//SET  A MINIMUM FUNDING VALUE IN USD 5$
+
+
 
 contract FundMe{
+
+    uint256 public minimumUSD=5;
     function fund() public payable{
         //ALLOW USERS TO SEND MONEY
         //SET A MIN AMOUNT
         //1.HOW DO WE SEND ETH TO THIS ACOCUNT
 
-        require(msg.value > 1e18,"DIDNT SENT ENOUGH ETH");  //1e18=1ETH=1000000000000=1*10**18
+        require(msg.value >= minimumUSD,"DIDNT SENT ENOUGH ETH");  //1e18=1ETH=1000000000000=1*10**18
 
         //what is reverrt
         //undo any actions that have been doen,and send the remainig gas back
